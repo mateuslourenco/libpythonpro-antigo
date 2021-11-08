9f00040002ce00d475b1302ca9904441ceb6515c
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-07 21:53:45 -0300

    Inserido campo email na classe Usuario

--- libpythonpro_m/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro_m/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,8 +2,8 @@ from libpythonpro_m.spam.enviador_de_email import Enviador
 from libpythonpro_m.spam.main import EnviadorDeSpam
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador)
+def test_qde_envio_de_spam(sessao):
+    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
     enviador_de_spam.enviar_emails(
         'mateusalourenco55@outlook.com',
         'Curso Python Pro',
