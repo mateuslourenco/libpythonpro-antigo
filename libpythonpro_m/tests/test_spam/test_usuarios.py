89731ebbaab863a059f7b78202c002192c340951
Mateus Lourenco <mateuslourenco55@outlook.com>
2021-11-07 22:19:01 -0300

    Implementado teste quantidade envio de spam

--- libpythonpro_m/tests/test_spam/test_usuarios.py
+++ libpythonpro_m/tests/test_spam/test_usuarios.py
@@ -47,7 +47,7 @@ def test_salvar_usuario(sessao):
 def test_listar_usuarios(sessao):
     usuarios = [
         Usuario(nome='Mateus', email='mateuslourenco55@outlook.com'),
-        Usuario(nome='Rafael', email='mateuslourenco55@outlook.com')
+        Usuario(nome='Rafael', email='mateusalourenco@gmail.com')
     ]
     for usuario in usuarios:
         sessao.salvar(usuario)
